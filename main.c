ee8b85afcefe8199490970fab809506d340f2bf5
dang0x5f <d4n9xcv@gmail.com>
2025-06-21 21:27:00 -0400

    added create_button()

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <X11/Xlib.h>
 
 #define OO_BTTN_IMPLEMENTATION
 #include "oo_bttn.h"
+
+// TODO: organize font setup
+char *font_name = "fixed";
+XFontStruct *font_info;
+void font_setup(Display *display)
+{
+    font_info = XLoadQueryFont(display,font_name);
+    if(!font_info) 
+        exit(-1);
+}
 
 int main(void)
 {
@@ -25,11 +36,21 @@ int main(void)
     Window window = XCreateWindow(display, root, 0, 0, 400, 300, 5, 
                         depth, class, visual, valuemask, &attributes);
 
+    font_setup(display);
+
+    create_button(display,&window,screen_num,font_info);
+
     XMapWindow(display,window);
     XSync(display,false);
     XEvent event;
     while(1){
+        /* oo_button *button = NULL; */
         XNextEvent(display,&event);
+        switch(event.type){
+            /* case Expose: */
+            /*     if(button) button_expose(button,&event); */
+            /*     break; */
+        }
         if(event.xkey.keycode == 9) break;
     }
 
--- oo_bttn.h
+++ oo_bttn.h
@@ -10,4 +10,32 @@ typedef struct {
     size_t label_len;
 } oo_button;
 
+void create_button(Display*,Window*,int,XFontStruct*);
+void draw_button(Display*,oo_button*);
+
 #endif // OO_BTTN_H
+
+// oo_button 
+void
+create_button(Display *display, Window *parent, int screen_num, 
+              XFontStruct *font)
+{
+    int depth = DefaultDepth(display,screen_num);
+    unsigned int class = InputOutput;
+    Visual *visual = DefaultVisual(display,screen_num);
+    unsigned long valuemask = 0;
+    valuemask |= CWBackPixel;
+    valuemask |= CWBorderPixel;
+    valuemask |= CWEventMask;
+    XSetWindowAttributes attributes = {
+        .background_pixel = 0xbbbbbb,
+        .border_pixel = 0x666666,
+        .event_mask = KeyPressMask|EnterWindowMask|LeaveWindowMask,
+    };
+
+    Window subwin = XCreateWindow(display,*parent,0,0,100,20,2, depth,
+                                  class,visual,valuemask,&attributes);
+    XSetFont(display,DefaultGC(display,screen_num),font->fid);
+    XDrawString(display,subwin,DefaultGC(display,screen_num),0,0,"Button",6);
+    XMapWindow(display,subwin);
+}
